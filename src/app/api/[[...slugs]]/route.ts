c3365dca629ae95339ecabea7180d1a15bb6cceb
avishekdas2018 <dasavishek2013@gmail.com>
2025-12-09 13:49:56 +0530

    Initial commit

---
+++ src/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,12 @@
+import { Elysia, t } from 'elysia'
+
+const app = new Elysia({ prefix: '/api' })
+  .get('/user', { user: { name: "Avishek" } })
+
+
+
+export const GET = app.fetch
+export const POST = app.fetch
+
+
+export type App = typeof app
